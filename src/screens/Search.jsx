5696adbfcb0ab3a45b05307a883003a30f10e190
Mirna Marinho Andrade <amimarinhoux@gmail.com>
2025-11-25 18:16:21 -0300

    Ajustando nomenclatura da pasta

--- src/screens/Search.jsx
+++ src/screens/Search.jsx
@@ -1,21 +1,28 @@
-import { Button} from '../common-componentes/Button/button';
-import { Card } from '../common-componentes/Card/Card';
-import { Description } from '../common-componentes/Description/Description';
-import { Header } from '../common-componentes/Header/Header';
-import { HeadingOne } from '../common-componentes/HeadingOne/HeadingOne';
-import { HeadingTwo } from '../common-componentes/HeadingTwo/HeadingTwo';
-import { SearchField } from '../common-componentes/SearchField/SearchField';
-import { Subtitle } from '../common-componentes/Subtitle/Subtitle';
+import { Button } from "../common-components/Button/Button.jsx";
 
 export function Search() {
-	return (
-		<div>
-			<Header />
-			<SearchField  placeholder="Digite um nome de herói ou heroína"/>
-			<Button ghost={true}>Buscar</Button>
-			<Card> 
-				<HeadingOne></HeadingOne>
-			</Card>
-		</div>
-	);
+  return (
+    <div>
+      <Header />
+
+      <SearchField placeholder="Digite um nome de herói ou heroína" />
+
+      <Button ghost={true}>Buscar</Button>
+
+      <Card>
+        <HeadingOne>Heading One</HeadingOne>
+        <HeadingTwo>Heading Two</HeadingTwo>
+
+        <div>
+          <Subtitle>Subtitle</Subtitle>
+        </div>
+
+        <Description>Description</Description>
+
+        <div>
+          <Caption>Caption</Caption>
+        </div>
+      </Card>
+    </div>
+  );
 }
